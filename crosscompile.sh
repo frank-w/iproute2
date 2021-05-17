487b40cdfb06c63a1671ea28fd741eaec08d01f2
Frank Wunderlich <frank-w@public-files.de>
2021-05-17 11:15:15 +0200

    add basic script for crosscompile
    There are issues with netem local table generation
    so disable netem and some others in Makefile

---
+++ crosscompile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+make clean
+CROSS_COMPILE=arm-linux-gnueabihf-
+#CROSS_COMPILE=aarch64-linux-gnu-
+make -j8 CC="${CROSS_COMPILE}gcc" LD="${CROSS_COMPILE}-ld" LDFLAGS=-static
+file ip/ip
