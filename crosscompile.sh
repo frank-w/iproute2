7e1c41ffe1d6d8cc9f411535e8f4be6c72dbbb73
Frank Wunderlich <frank-w@public-files.de>
2021-05-18 17:28:34 +0200

    crosscompile: add install to local directory

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 make clean
-CROSS_COMPILE=arm-linux-gnueabihf-
-#CROSS_COMPILE=aarch64-linux-gnu-
+ARCH=arm
+#ARCH=aarch64
+if [[ "$ARCH"=="arm" ]];then
+	CROSS_COMPILE=arm-linux-gnueabihf-
+elif [[ "$ARCH"=="aarch64" ]];then
+	CROSS_COMPILE=aarch64-linux-gnu-
+fi
+echo $CROSS_COMPILE
 make -j8 CC="${CROSS_COMPILE}gcc" LD="${CROSS_COMPILE}-ld" HOSTCC=gcc
 file ip/ip
+
+PRFX=$(pwd)/output_$ARCH
+mkdir $PRFX
+make PREFIX=$PRFX SBINDIR=$PRFX/sbin ARPDDIR=$PRFX/var/lib/arpd CONFDIR=$PRFX/etc/iproute2 install
